c45b2ef411b2745e8256c3b964c41191620b6111
guoqiaolongfei <guoqiaolongfei123@gmail.com>
2021-04-28 16:09:24 -0400

    Initialized Structure

---
+++ leomail-product/src/test/java/com/leo/leomail/leomailproduct/LeomailProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leo.leomail.leomailproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeomailProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
